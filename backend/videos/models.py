af6486541745954060cac14ca7451a5ef3c7fd09
Dan Langan <danlangan@Dans-MacBook-Pro-2.local>
2022-09-06 19:21:20 -0500

    added the video app, might have to delete it since I got a message from pascal saying that were only storing info on comments in the database... but there's my models file for the videos

--- backend/videos/models.py
+++ backend/videos/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from authentication.models import User
 
 # Create your models here.
 
 class Video(models.Model):
-    video_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=300)
     release_date = models.DateField()
