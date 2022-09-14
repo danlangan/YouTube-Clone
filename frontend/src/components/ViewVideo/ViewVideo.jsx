1b8cff9327cb104b470dbcf4a0ac462ef982f0d5
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-09-14 14:10:03 -0500

    re named a couple of things and re-organized some of the URLs and the way that the home page will map over the video objects

--- frontend/src/components/ViewVideo/ViewVideo.jsx
+++ frontend/src/components/ViewVideo/ViewVideo.jsx
@@ -5,7 +5,7 @@ const [video, seeVideo] = useState([])
 
 const ViewVideo = async () => {
     try {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q={SEARCH QUERY HERE}&key=AIzaSyDdEHMH6ijhDun3kNDmWuWh1jUCbr0WOjI`);
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q={user.search}&key=AIzaSyDdEHMH6ijhDun3kNDmWuWh1jUCbr0WOjI`);
         seeVideo(response.data);
     } catch (error) {
         console.log(error.message);
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -8,31 +8,29 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [user, token] = useAuth(); // this will be needed on your video player page to get user info
+  const [videos, setVideos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("bob ross");
+
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchSearchResults = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=AIzaSyDdEHMH6ijhDun3kNDmWuWh1jUCbr0WOjI`);
+        setVideos(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchCars();
+    fetchSearchResults();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      {videos &&
+        videos.map((video) => (
+          <p key={video.videoid}>
+            {video.name} {car.discripion}
           </p>
         ))}
     </div>
