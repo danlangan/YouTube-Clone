1f6c94e97670e7d10aed2f302de0a36afe3bc791
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2022-09-29 16:47:31 -0500

    added the api key and made sure that it was in the gitignore file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // General Imports
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { KEY } from './localKey';
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
