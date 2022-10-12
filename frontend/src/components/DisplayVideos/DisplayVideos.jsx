3a214d98efc3b19b068ef73c43662565c3b251aa
Dan Langan <danlangan@Dans-MacBook-Pro.local>
2022-10-11 20:17:56 -0500

    edited the display videos section and am looking to trouble shoot to re-direct how my current application to make it work again... lol

--- frontend/src/components/DisplayVideos/DisplayVideos.jsx
+++ frontend/src/components/DisplayVideos/DisplayVideos.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { KEY } from '../localKey';
 
@@ -27,7 +27,7 @@ async () => {
                     return false;
                 }
         });
-        props.setSearchTerm(response);
+        event.setSearchTerm(response);
     }
 
     return (
